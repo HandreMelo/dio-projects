70ffd6d7da2a6671c6c3b492c4ca1153ba68e9d3
André Melo <34012482+HandreMelo@users.noreply.github.com>
2020-08-03 00:35:02 -0300

    Add files via upload

---
+++ techchalange-dio-carrefour/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dio_chatbot_carrefour
+{
+    public static class Configuration
+    {
+        public readonly static string BotToken = "1158503808:AAEq2-cfGLV7YKMig8oWr170bhMKaa9_n5k";
+
+#if USE_PROXY
+        public static class Proxy
+        {
+            public readonly static string Host = "localhost";
+            public readonly static int Port = 8080;
+        }
+#endif
+    }
+}
